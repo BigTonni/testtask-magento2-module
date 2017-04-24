7ca83a922116496af6ef75b1534a48b0871c656d
Anton Shulga <shandm85@gmail.com>
2017-04-24 16:21:28 +0300

    First commit

---
+++ app/code/Test/TestTask/Ui/Component/Listing/Columns/ProductStock.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Test\TestTask\Ui\Component\Listing\Columns;
+
+class ProductStock extends \Magento\Ui\Component\Listing\Columns\Column {
+
+     /**
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
+     */
+     
+    public function __construct(
+        \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
+        \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ){
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource) {
+        if (isset($dataSource['data']['items'])) {
+
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $StockState = $objectManager->get('\Magento\CatalogInventory\Api\StockStateInterface');
+            $StockRegistry = $objectManager->get('Magento\CatalogInventory\Api\StockRegistryInterface');
+           
+            foreach ($dataSource['data']['items'] as $key => $item) {
+                $productStockObj = $StockRegistry->getStockItem($item['entity_id']);
+                $dataSource['data']['items'][$key]['product_stock'] = $StockState->getStockQty($productStockObj['product_id'], $productStockObj['website_id']);
+            }
+        }
+
+        return $dataSource;
+    }
+}
